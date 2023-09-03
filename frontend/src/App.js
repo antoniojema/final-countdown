7c37b55e78b4513b7d5c59acdb3d83ba310044ca
Antonio (laptop) <antoniojema@ugr.es>
2023-09-03 12:26:08 -0400

    Deployed
    Cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 // Time in Spain: 5:00 (UTC + 2)
-const initDate = new Date(2023, 7, 30, 3, 0)
+const initDate = new Date(Date.UTC(2023, 7, 30, 3, 0))
 // Time in Orlando when arriving: 19:00 (UTC - 3 due to daylight savings)
-const finishDate = new Date(2023, 11, 1, 22, 0)
+const finishDate = new Date(Date.UTC(2023, 11, 1, 22, 0))
 // Total time in milliseconds from the previous dates
 const totalTime = finishDate.getTime() - initDate.getTime()
 
