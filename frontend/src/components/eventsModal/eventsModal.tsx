8ce51a601c6b860034d2c10314e29dc3cf723f8d
Juanma Ucero <juanmaucero97@gmail.com>
2023-11-04 18:11:41 +0100

    modal and collapse

--- frontend/src/components/eventsModal/eventsModal.tsx
+++ frontend/src/components/eventsModal/eventsModal.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { EVENTS } from '../../utils/api'
 import { formatUTCDate, getModalId } from '../../utils/utils'
 
-export class EventsModal extends React.Component<{city : string, date : Date},{}> {
-  constructor(props : {city : string, date : Date}) {
+export class EventsModal extends React.Component<{ city: string, date: Date }, {}> {
+  constructor(props: { city: string, date: Date }) {
     super(props)
   }
 
@@ -15,21 +15,36 @@ export class EventsModal extends React.Component<{city : string, date : Date},{}
   render() {
     const id = getModalId(this.props.city, this.props.date)
     const City = this.props.city.charAt(0).toUpperCase() + this.props.city.slice(1)
-    
+
     return (
       <div className="modal fade" id={id} tabIndex={-1} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id="exampleModalLabel">{City} {formatUTCDate(this.props.date)}</h5>
-              <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"></span>
               </button>
             </div>
             <div className="modal-body">
               {this.getEvents()}
             </div>
             <div className="modal-footer">
+              <p>
+              <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                Button with data-bs-target
+              </button>
+              </p>
+              <div className="collapse" id="collapseExample">
+                <div className="card card-body">
+                  Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                </div>
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text">$</span>
+                <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
+                <span className="input-group-text">.00</span>
+              </div>
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
               <button type="button" className="btn btn-primary">Create new event</button>
             </div>
